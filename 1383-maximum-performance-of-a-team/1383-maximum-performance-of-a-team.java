fb833461e4a5b8a3ac4a03fd353fcbc2c2af4a69
Changmoo Moon <public.stevemoon@gmail.com>
2022-10-11 15:23:51 +0900

    Time: 99 ms (63.48%), Space: 70.4 MB (24.98%) - LeetHub

---
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+        int[][] a = new int[n][2];
+        for(int i = 0; i < n; ++i) {
+            a[i][0] = speed[i];
+            a[i][1] = efficiency[i];
+        }
+        
+        Arrays.sort(a, (p1, p2) -> p2[1] - p1[1]);
+        
+        long speedSum = 0;
+        long ret = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < n; ++i) {
+            while(pq.size() >= k) speedSum -= pq.poll();
+            
+            pq.add(a[i][0]);
+            speedSum += a[i][0];
+            ret = Math.max(speedSum * a[i][1], ret);
+        }
+        
+        return (int)(ret % ((int)1e9 + 7));
+    }
+}
