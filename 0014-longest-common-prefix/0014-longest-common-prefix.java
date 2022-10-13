f9cf318351ff6bcfe571c3d1b2f2b4fa2242433f
Changmoo Moon <public.stevemoon@gmail.com>
2022-10-14 07:24:17 +0900

    Time: 1 ms (92.44%), Space: 42.2 MB (42.94%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,22 +4,15 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         if(strs.length == 0) return "";
         
-        Arrays.sort(strs, (a, b) -> a.length() - b.length());
-        String ret = strs[0];
-        
-        for(int i = 1; i < strs.length; ++i) {
-            int endIdx = cmp(ret, strs[i]);
-            ret = ret.substring(0, endIdx);
+        for(int i = 0; i < strs[0].length(); ++i) {
+            char ch = strs[0].charAt(i);
+            for(int j = 1; j < strs.length; ++j) {
+                if(strs[j].length() == i || strs[j].charAt(i) != ch) {
+                    return strs[0].substring(0, i);
+                }
+            }
         }
         
-        return ret;
-    }
-    
-    int cmp(String s1, String s2) {
-        for(int i = 0; i < s1.length(); ++i) {
-            if(s1.charAt(i) != s2.charAt(i)) return i;
-        }
-        
-        return s1.length();
+        return strs[0];
     }
 }
