c9715e017cbc6a43da48ccd93a90f2c1f79cdeae
Changmoo Moon <public.stevemoon@gmail.com>
2022-10-16 23:11:03 +0900

    Time: 11 ms (49.74%), Space: 46.9 MB (25.96%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -2,42 +2,22 @@ import java.util.*;
 
 class Solution {
     public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>() {{
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
+        }};
+        
         int ret = 0;
         for(int i = 0; i < s.length(); ++i) {
-            char ch = s.charAt(i);
-            if(ch == 'I') {
-                if(s.indexOf("IV", i) == i) {
-                    ret += 4;
-                    ++i;
-                } else if(s.indexOf("IX") == i) {
-                    ret += 9;
-                    ++i;
-                } else ret += 1;
-            } else if(ch == 'V') {
-                ret += 5;
-            } else if(ch == 'X') {
-                if(s.indexOf("XL", i) == i) {
-                    ret += 40;
-                    ++i;
-                } else if(s.indexOf("XC", i) == i) {
-                    ret += 90;
-                    ++i;
-                } else ret += 10;
-            } else if(ch == 'L') {
-                ret += 50;
-            } else if(ch == 'C') {
-                if(s.indexOf("CD", i) == i) {
-                    ret += 400;
-                    ++i;
-                } else if(s.indexOf("CM", i) == i) {
-                    ret += 900;
-                    ++i;
-                } else ret += 100;
-            } else if(ch == 'D') {
-                ret += 500;
-            } else if(ch == 'M') {
-                ret += 1000;
-            }
+            int val = map.get(s.charAt(i));
+            if(i + 1 < s.length() && map.get(s.charAt(i + 1)) > val) {
+                ret -= val;
+            } else ret += val;
         }
         
         return ret;
