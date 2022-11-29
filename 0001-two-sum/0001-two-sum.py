3062a8d9b83205962f40f0a684170f4d8471a66d
Changmoo Moon <public.stevemoon@gmail.com>
2022-11-29 15:24:14 +0900

    Time: 2224 ms (23.26%), Space: 14.9 MB (96.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,6 +2,4 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i, val in enumerate(nums): # 0
             if target - val in nums[i + 1:]:
-                fi = i
-                se = nums[i + 1:].index(target - val) + (i + 1)
-                return [fi, se]
+                return [i, nums[i + 1:].index(target - val) + i + 1]
