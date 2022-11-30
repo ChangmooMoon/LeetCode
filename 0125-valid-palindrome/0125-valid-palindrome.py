6959d5a325c53d002fc033cdac5e42996ce7907f
Changmoo Moon <public.stevemoon@gmail.com>
2022-11-30 20:47:27 +0900

    Time: 81 ms (67.66%), Space: 15.2 MB (29.12%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(filter(str.isalnum, s.lower()))
+        print(s)
+        
+        st = 0
+        en = len(s) - 1
+        while st <= en:
+            if s[st] != s[en]:
+                return False
+            st += 1
+            en -= 1
+        return True
