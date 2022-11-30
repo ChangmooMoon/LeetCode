aad43cd7c0bd594bec51246f073610afa2486fdd
Changmoo Moon <public.stevemoon@gmail.com>
2022-11-30 20:47:38 +0900

    Time: 72 ms (74.97%), Space: 15.1 MB (29.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = ''.join(filter(str.isalnum, s.lower()))
-        print(s)
         
         st = 0
         en = len(s) - 1
