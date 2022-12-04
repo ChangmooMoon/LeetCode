4b57c6abb8f570595ddcb5189486d152df595cda
Changmoo Moon <public.stevemoon@gmail.com>
2022-12-04 10:38:48 +0900

    Time: 1536 ms (18.42%), Space: 9 MB (78.92%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return bf(0, 0, nums, target);
+    }
+    
+    int bf(int idx, int sum, vector<int>& nums, int target) {
+        if(idx == nums.size()) {
+            return sum == target ? 1 : 0;
+        }
+        
+        return bf(idx + 1, sum + nums[idx], nums, target)
+            + bf(idx + 1, sum - nums[idx], nums, target);
+    }
+};
