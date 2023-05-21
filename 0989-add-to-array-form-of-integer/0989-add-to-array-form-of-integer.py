b4633761dc543d23d06d603e6213c579c4b19549
Changmoo Moon <public.ellerymoon@gmail.com>
2023-05-21 21:41:48 +0900

    Time: 490 ms (21.33%), Space: 17.3 MB (22.19%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -0,0 +1,13 @@
+import sys
+sys.set_int_max_str_digits(0)
+
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        digit, curr = 1, 0
+        
+        for n in num[::-1]:
+            curr += n * digit
+            digit *= 10
+        
+        str(curr + k)
+        return map(int, list(str(curr + k)))
