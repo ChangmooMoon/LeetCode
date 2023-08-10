48e8e2c73ed8a60fd10b718b465fe8a58237a567
Changmoo Moon <public.ellerymoon@gmail.com>
2023-08-11 01:18:21 +0900

    Time: 31 ms (89.38%), Space: 13.9 MB (36.95%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def findRedundantConnection(self, edges):
+        p = [_ for _ in range(1001)]
+        
+        def Find(p, x):
+            if p[x] != x:
+                p[x] = Find(p, p[x])
+            return p[x]
+        
+        def Union(p, a, b):
+            a, b = Find(p, a), Find(p, b)
+            if a < b:
+                p[b] = a
+            else:
+                p[a] = b
+        
+        for u, v in edges:
+            if Find(p, u) == Find(p, v):
+                return [u, v]
+            Union(p, u, v)
+                
+        
+            
+            
+        
+        
+        
+        
+"""
+:type edges: List[List[int]]
+:rtype: List[int]
+"""
+'''
+1. undirected graph, tree + 1 edge, n 1000
+2. uf 사이클 체크 1000 * (1000 + 1000) = 200만
+'''	
