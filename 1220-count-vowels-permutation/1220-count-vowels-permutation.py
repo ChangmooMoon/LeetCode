d15f8b39ce9e23dd8040452626f2f5af23fd44a2
Changmoo Moon <public.ellerymoon@gmail.com>
2023-08-27 16:35:57 +0900

    Time: 199 ms (77.88%), Space: 16.5 MB (68.79%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,33 +1,11 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
         MOD = 10**9 + 7
-        d = [[0 for _ in range(n + 1)] for _ in range(5)] # d[i][j] = count of vowels permutation and str[j] is vowel, len is i
-        A, E, I, O, U = 0, 1, 2, 3, 4
-        
-        d[A][1], d[E][1], d[I][1], d[O][1], d[U][1] = 1, 1, 1, 1, 1
-        
-        for j in range(2, n + 1):
-            if d[A][j - 1]:
-                d[E][j] += d[A][j - 1] 
-            if d[E][j - 1]:
-                d[A][j] += d[E][j - 1]
-                d[I][j] += d[E][j - 1]
-            if d[I][j - 1]:
-                d[A][j] += d[I][j - 1] 
-                d[E][j] += d[I][j - 1]
-                d[O][j] += d[I][j - 1]
-                d[U][j] += d[I][j - 1]
-            if d[O][j - 1]:
-                d[I][j] += d[O][j - 1]
-                d[U][j] += d[O][j - 1]
-            if d[U][j - 1]:
-                d[A][j] += d[U][j - 1] 
-        
-        ans = 0
-        for i in range(5):
-            ans += d[i][n]
-        
-        return ans % MOD
+        a, e, i, o, u = 1, 1, 1, 1, 1
+        for _ in range(2, n + 1):
+            a, e, i, o, u = e + i + u, a + i, e + o, i, i + o
+            
+        return (a + e + i + o + u) % MOD
                 
 """
 1. n 20000,  aeiou -> permutation
@@ -35,7 +13,9 @@ class Solution:
 try1 - 5^20000 pruning? no dfs
 try2 - dp?
 
-{'a': 0, 'e': 1, 'i': 2, 'o': 3, 'u', 4}
-
-20001 * 5 2d arr, when row == n, meet the conditions
+a -> e
+e -> a, i
+i -> a, e, o, u
+o -> i, u
+u -> a
 """
