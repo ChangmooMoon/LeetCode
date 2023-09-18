6721f2065cb26d96a2a30a4acceee61c1086aae8
Changmoo Moon <public.ellerymoon@gmail.com>
2023-09-18 23:08:44 +0900

    Time: 927 ms (68.66%), Space: 19.5 MB (74.63%) - LeetHub

---
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minKBitFlips(self, nums: List[int], k: int) -> int:
+        N = len(nums)
+        ret = 0
+        
+        a = [0] * N # toggle
+        flag = 0 # off, on
+        
+        for i in range(N): 
+            if a[i] == 1: # if bit is 1, 
+                flag ^= 1
+            
+            if nums[i] ^ flag == 0: # if either one is 0
+                ret += 1
+                flag ^= 1
+                
+                if i + k < N:
+                    a[i + k] = 1
+                elif i + k > N:
+                    return -1
+        return ret
+                
+        
+"""
+1. n = 10^5, less than O(N^2) time complexity
+
+"""
