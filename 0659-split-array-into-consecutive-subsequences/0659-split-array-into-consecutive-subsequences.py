520eea0de6e974252d775958e2130ecd2ed12f38
Changmoo Moon <public.ellerymoon@gmail.com>
2023-10-04 19:11:02 +0900

    Time: 589 ms (12.68%), Space: 17.7 MB (30.07%) - LeetHub

---
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isPossible(self, nums: List[int]) -> bool:
+        a, b, c = Counter(), Counter(), Counter()
+        
+        for el in nums:
+            if a[el - 1]:
+                a[el - 1] -= 1
+                b[el] += 1
+                continue
+                
+            if b[el - 1]:
+                b[el - 1] -= 1
+                c[el] += 1
+                continue
+                
+            if c[el - 1]:
+                c[el - 1] -= 1
+                c[el] += 1
+                continue
+                
+            a[el] += 1
+                
+        return a.total() == 0 and b.total() == 0
+"""
+1. n 10000, less than O(n^2) nlogn, n
+"""
+            
+        
+        
