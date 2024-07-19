754d5774198432b87737dcf2b184df71e041800a
Changmoo Moon <public.ellerymoon@gmail.com>
2024-07-19 11:27:53 +0900

    Time: 2755 ms (51.54%), Space: 201.5 MB (19.23%) - LeetHub

---
+++ 1808-stone-game-vii/1808-stone-game-vii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def stoneGameVII(self, stones: List[int]) -> int:
+        d = [[-1] * len(stones) for _ in range(len(stones))]
+        
+        def solve(i, j, rest):
+            if i == j:
+                return 0
+
+            if j - i == 1:
+                return max(stones[i], stones[j])
+            
+            if d[i][j] != -1:
+                return d[i][j]
+
+            pick_left = rest - stones[i] - solve(i + 1, j, rest - stones[i])
+            pick_right = rest - stones[j] - solve(i, j - 1, rest - stones[j])
+            d[i][j] = max(pick_left, pick_right)
+            return d[i][j]
+
+        return solve(0, len(stones) - 1, sum(stones))
