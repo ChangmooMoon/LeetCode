a10aee0ec56dfec986099f3f569666d0c38d952d
Changmoo Moon <public.ellerymoon@gmail.com>
2024-07-28 17:28:19 +0900

    Time: 623 ms (29.14%), Space: 31.9 MB (67.22%) - LeetHub

--- 2538-minimum-cost-to-make-array-equal/2538-minimum-cost-to-make-array-equal.py
+++ 2538-minimum-cost-to-make-array-equal/2538-minimum-cost-to-make-array-equal.py
@@ -3,11 +3,7 @@ class Solution:
         def getCost(target): 
             # a <= c <= b, cost = abs(a - c) * x + abs(b - c) * y
             return sum(abs(nums[i] - target) * cost[i] for i in range(len(nums)))
-
-        N = len(nums)
-        if N == 1:
-            return 0
-
+            
         lo, hi = min(nums), max(nums)
         while lo < hi:
             mid = (lo + hi) // 2
