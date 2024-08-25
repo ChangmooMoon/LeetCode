34de3f4fdea9d6edd9fa6222c853991d428e82f7
Changmoo Moon <public.ellerymoon@gmail.com>
2024-08-25 16:59:25 +0900

    Time: 243 ms (33.26%), Space: 17 MB (38.99%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -4,11 +4,14 @@ class Solution:
         d = [[n] for n in nums] # d[i] = subset at i, longest
         ans = []
 
-        for i in reversed(range(len(nums))):
+        for i in reversed(range(len(nums))): # nums[i] < nums[j]
             for j in range(i + 1, len(nums)):
                 if nums[j] % nums[i] == 0:
                     res = [nums[i]] + d[j]
-                    d[i] = res if len(res) > len(d[i]) else d[i]
-            ans = d[i] if len(d[i]) > len(ans) else ans
+                    if len(res) > len(d[i]):
+                        d[i] = res
+
+            if len(d[i]) > len(ans):
+                ans = d[i]
             
         return ans
