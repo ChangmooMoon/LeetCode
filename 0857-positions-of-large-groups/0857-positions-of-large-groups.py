6747c44d5a5d8e7c3d10565f74e295d2d63e5457
Changmoo Moon <public.ellerymoon@gmail.com>
2025-03-01 01:09:21 +0900

    Time: 0 ms (100%), Space: 17.6 MB (87.28%) - LeetHub

--- 0857-positions-of-large-groups/0857-positions-of-large-groups.py
+++ 0857-positions-of-large-groups/0857-positions-of-large-groups.py
@@ -1,16 +1,13 @@
 class Solution:
     def largeGroupPositions(self, s: str) -> List[List[int]]:
         ans = []
-        
-        st = 0
-        for i in range(len(s)):
-            if s[i] == s[st]:
-                continue
-            if i - st >= 3:
-                ans.append([st, i - 1])
-            st = i
-
-        if len(s) - st >= 3:
-            ans.append([st, len(s) - 1])
-             
+        s += ' '
+        p, q = 0, 0
+        for i in range(1, len(s)):
+            if s[i] == s[p]:
+                q = i
+            else:
+                if q - p >= 2:
+                    ans.append([p, q])
+                p = q = i
         return ans
