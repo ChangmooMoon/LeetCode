c04533644f238d10e4d2aba1d4711750f4725741
Changmoo Moon <public.ellerymoon@gmail.com>
2025-06-09 09:26:21 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minScoreTriangulation(self, values: List[int]) -> int:
+        
+        def dfs(i, j):
+            tmp = float('inf')
+            for k in range(i+1, j):
+                tmp = min(tmp, values[i] * values[k] * values[j] + dfs(i,k) + dfs(k,j))
+            return tmp if tmp != float('inf') else 0
+
+        return dfs(0, len(values)-1)
